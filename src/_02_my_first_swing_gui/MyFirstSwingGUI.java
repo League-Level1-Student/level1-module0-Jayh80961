700adf91fefc81d8a57c537673c51b066d890ee8
Jayh80961 <59904920+Jayh80961@users.noreply.github.com>
2020-08-05 10:26:46 +0900

    did very well

--- src/_02_my_first_swing_gui/MyFirstSwingGUI.java
+++ src/_02_my_first_swing_gui/MyFirstSwingGUI.java
@@ -16,41 +16,42 @@ public class MyFirstSwingGUI {
 	public void run() {
 
 		// 1. Declare and initialize an object of the JFrame class
-
+		JFrame jframe = new JFrame();
 		// 2. Set your JFrame object to be visible
-
+		jframe.setVisible(true);
+		jframe.setSize(700, 700);
 		// 3. Open MyFirstSwingGUIRunner and run your program. Do you see your window? It's probably very
 		// small.
-
+		
 		// 4. Use your JFrame to call setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
-
+		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// 5. Declare and initialize an object of the JPanel class
-
+		JPanel jpanel = new JPanel();
 		// 6. Declare and initialize an object of the JLabel class
-
+		JLabel jlabel = new JLabel();
 		// 7. Set the text of the JLabel to a lovely greeting
-
+		jlabel.setText("Hello!!!");
 		// 8. Add the JPanel object to the JFrame
-
+		jframe.add(jpanel);
 		// 9. Add the JLabel object to the JPanel
-
+		jpanel.add(jlabel);
 		// 10. Pack your JFrame
-
+		jframe.pack();
 		// 11. Run your program again. Do you see your message?
-
+		//yes
 		// 12. Use your JLabel to call setIcon(loadImage())
-
+		jlabel.setIcon(loadImage());
 		// 13. Re-pack the JFrame object
-
+		jframe.pack();
 		// 14. Run the program one more time. Do you see the image?
-
+		//yes
 	}
 
 	public ImageIcon loadImage() {
 		try {
 			return new ImageIcon(ImageIO.read(new MyFirstSwingGUI().getClass().getResourceAsStream("java.png")));
 		} catch (IOException e) {
-			e.printStackTrace()
+			e.printStackTrace();
 			return null;
 		}
 	}
--- src/_03_photo_quiz/PhotoQuiz.java
+++ src/_03_photo_quiz/PhotoQuiz.java
@@ -20,41 +20,53 @@ public class PhotoQuiz {
 
 		JFrame quizWindow = new JFrame();
 		quizWindow.setVisible(true);
+		quizWindow.setSize(700, 700);
 		// This will make sure the program exits when you close the window
 		quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		// 1. find an image on the internet, and put its URL in a String
 		// variable (from your browser, right click on the image, and select
 		// “Copy Image Address”)
-
+		
 		// 2. create a variable of type "Component" that will hold your image
-
+		String component = "https://lh3.googleusercontent.com/vA4tG0v4aasE7oIvRIvTkOYTwom07DfqHdUPr6k7jmrDwy_qA_SonqZkw6KX0OXKAdk"; 
+		
 		// 3. use the "createImage()" method below to initialize your Component
-
+		Component c = createImage(component);
 		// 4. add the image to the quiz window
-
+		quizWindow.add(c);
 		// 5. call the pack() method on the quiz window
-
+		quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+		String answer = JOptionPane.showInputDialog("When was the first youtube video made? (answer like two thousand twelve)");
 		// 7. print "CORRECT" if the user gave the right answer
-
+		if(answer.equalsIgnoreCase("two thousand three")) {
+			JOptionPane.showMessageDialog(null, "CORRECT");
+		}
 		// 8. print "INCORRECT" if the answer is wrong
-
+		else {
+			JOptionPane.showMessageDialog(null, "INCORRECT");
+		}
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
-
+		quizWindow.remove(c);
 		// 10. find another image and create it (might take more than one line
 		// of code)
-
+		String image = "https://www.themonitor.com/wp-content/uploads/sites/60/2019/03/Nine-banded-Armadillo-Photo-by-Anita-Westervelt.jpg";
 		// 11. add the second image to the quiz window
-
+		Component i = createImage(image);
+		quizWindow.add(i);
 		// 12. pack the quiz window
-
+		quizWindow.pack();
 		// 13. ask another question
-
+		String quiz = JOptionPane.showInputDialog("What is this animal's name?");
 		// 14+ check answer, say if correct or incorrect, etc.
-
+		if(quiz.equalsIgnoreCase("armadillo")) {
+			JOptionPane.showMessageDialog(null, "CORRECT");
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "INCORRECT");
+		}
 	}
 
 	private Component createImage(String imageUrl) throws MalformedURLException {
